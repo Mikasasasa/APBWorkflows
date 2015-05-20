42e78096d2ad62aff4814286573ad43fe88eef3a
mm@oyatel.com <mm@oyatel.com>
2015-05-20 20:53:52 +0200

    Added TrainingMeetingService

---
+++ WorkFlowAPB/TrainingMeetingService/Models/ProposedDatesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TrainingMeetingService.Models
+{
+    public class ProposeDatesResponse
+    {
+        [DataMember]
+        public DateTime[] ProposedDateTimes { get; set; }
+    }
+}
---
+++ WorkFlowAPB/TrainingMeetingService/Models/SaveChangesToDietRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TrainingMeetingService.Models
+{
+    [DataContract]
+    public class SaveChangesToDietRequest
+    {
+        [DataMember]
+        public string NewDietText { get; set; }
+    }
+}
